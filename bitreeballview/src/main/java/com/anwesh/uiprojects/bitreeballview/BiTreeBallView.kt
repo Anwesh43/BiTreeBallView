74e17e96355875eb09717dcf00bc399a5acdf6ac
Anwesh43 <anweshthecool0@gmail.com>
2019-09-27 10:36:46 +0530

    adding Canvas extension method

--- bitreeballview/src/main/java/com/anwesh/uiprojects/bitreeballview/BiTreeBallView.kt
+++ bitreeballview/src/main/java/com/anwesh/uiprojects/bitreeballview/BiTreeBallView.kt
@@ -13,7 +13,7 @@ import android.app.Activity
 
 val levels : Int = 3
 val scGap : Float = 0.05f
-val strokeFactor : Int = 90
+val strokeFactor : Float = 90f /(levels + 1)
 val sizeFactor : Float = 3f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -22,4 +22,21 @@ val delay : Long = 45
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.gap() : Float {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    return Math.min(w, h) / (levels + 1)
+}
+fun Canvas.drawBallLine(i : Int, j : Int, scale : Float, paint : Paint) {
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = gap() / strokeFactor
+    val size : Float = gap() / sizeFactor
+    save()
+    drawCircle(0f, 0f, size, paint)
+    drawLine(0f, 0f, (i + 1) * gap(), 0f, paint)
+    drawLine(0f, 0f, 0f, (i + 1) * gap(), paint)
+    restore()
+}
 
