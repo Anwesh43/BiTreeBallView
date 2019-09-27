83f2cf7016fda6d1711e4bfad90dd43d3ef80335
Anwesh43 <anweshthecool0@gmail.com>
2019-09-27 10:18:37 +0530

    adding Constants and libraries

--- bitreeballview/src/main/java/com/anwesh/uiprojects/bitreeballview/BiTreeBallView.kt
+++ bitreeballview/src/main/java/com/anwesh/uiprojects/bitreeballview/BiTreeBallView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bitreeballview
 /**
  * Created by anweshmishra on 27/09/19.
  */
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+
+val levels : Int = 3
+val scGap : Float = 0.05f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 3f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 45
+
