e72a82a1095d9a7554177688a0cf35695e666fc4
Anwesh43 <anweshthecool0@gmail.com>
2019-09-27 10:39:09 +0530

    adding BiTreeBallView class

--- bitreeballview/src/main/java/com/anwesh/uiprojects/bitreeballview/BiTreeBallView.kt
+++ bitreeballview/src/main/java/com/anwesh/uiprojects/bitreeballview/BiTreeBallView.kt
@@ -40,3 +40,20 @@ fun Canvas.drawBallLine(i : Int, j : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class BiTreeBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
